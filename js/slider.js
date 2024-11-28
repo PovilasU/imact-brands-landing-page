ffb478de2b2d91fed9e7289ed2c054295f0b1664
PovilasU <povilas.urbonas85@gmail.com>
2024-11-28 09:47:29 +0200

    added faq section

--- js/slider.js
+++ js/slider.js
@@ -9,3 +9,19 @@ const swiper = new Swiper(".swiper-container", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+
+  document.querySelectorAll(".faq__question").forEach((button) => {
+    button.addEventListener("click", () => {
+      const answer = button.nextElementSibling;
+      button.classList.toggle("active");
+      if (button.classList.contains("active")) {
+        answer.style.maxHeight = answer.scrollHeight + "px";
+        button.querySelector(".faq__arrow").innerHTML = "&#9650;";
+      } else {
+        answer.style.maxHeight = 0;
+        button.querySelector(".faq__arrow").innerHTML = "&#9660;";
+      }
+    });
+  });
